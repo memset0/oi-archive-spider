0334d9e9fa2afa688b37b4f855954a231f84af6d
memset0 <memset0@outlook.com>
2020-09-21 14:03:52 +0800

    feat: first commit

---
+++ utils.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+module.exports = {
+	path(dir) {
+		return path.join(process.env.OIARCHIVE_PATH, dir);
+	},
+
+	readJsonFile(dir) {
+		return JSON.parse(fs.readFileSync(dir).toString());
+	},
+	writeJsonFile(dir, json) {
+		return fs.writeFileSync(dir, JSON.stringify(json));
+	},
+
+	url: {
+		concat(root, uri) {
+			if (!uri) return null;
+			if (uri.startsWith('http://') || uri.startsWith('https://')) return uri;
+			if (root.endsWith('/')) root = root.slice(0, -1);
+			if (uri.startsWith('/')) uri = uri.slice(1);
+			return root + '/' + uri;
+		}
+	},
+
+
+};
